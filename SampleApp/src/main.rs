ad24fc7a8694e4365d7d809cb0af66c2b0fd0203
Moodie <ghaddar0292@gmail.com>
2021-06-28 14:44:25 -0700

    added minimal implementation of wgpu with SDL

--- SampleApp/src/main.rs
+++ SampleApp/src/main.rs
@@ -1,11 +1,120 @@
 #![allow(non_snake_case)]
 
-use sdl2;
-use sdl2::pixels::Color;
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
 use std::time::Instant;
 use std::collections::HashSet;
+use futures::executor::block_on;
+
+struct Graphics {
+    surface: wgpu::Surface,
+    device: wgpu::Device,
+    queue: wgpu::Queue,
+    sc_desc: wgpu::SwapChainDescriptor,
+    swap_chain: wgpu::SwapChain,
+    size: (u32, u32),
+}
+
+impl Graphics {
+    async fn new(window: &sdl2::video::Window) -> Self {
+        let size = window.size();
+
+        // The instance is a handle to our GPU
+        // BackendBit::PRIMARY => Vulkan + Metal + DX12 + Browser WebGPU
+        let instance = wgpu::Instance::new(wgpu::BackendBit::PRIMARY);
+
+        // Surface is used to create the swap chain and adapter
+        let surface = unsafe { instance.create_surface(window) };
+
+        // Adapter is used to create the device and queue
+        let adapter = instance.request_adapter(
+            &wgpu::RequestAdapterOptions {
+                power_preference: wgpu::PowerPreference::default(),
+                compatible_surface: Some(&surface),
+            },
+        ).await.unwrap();
+
+        let (device, queue) = adapter.request_device(
+            &wgpu::DeviceDescriptor {
+                // Specify any extra gpu feature. You can get a list of features supported by your device using adapter.features(), or device.features().
+                // https://docs.rs/wgpu/0.7.0/wgpu/struct.Features.html
+                features: wgpu::Features::empty(),
+
+                // The limits field describes the limit of certain types of resource we can create.
+                // https://docs.rs/wgpu/0.7.0/wgpu/struct.Limits.html
+                limits: wgpu::Limits::default(),
+
+
+                label: None,
+            },
+            None,
+        ).await.unwrap();
+        
+        // Define and creating the swap_chain.
+        let sc_desc = wgpu::SwapChainDescriptor {
+            // The usage field describes how the swap_chain's underlying textures will be used. 
+            usage: wgpu::TextureUsage::RENDER_ATTACHMENT,
+            // Defines how the swap_chains textures will be stored on the gpu
+            format: adapter.get_swap_chain_preferred_format(&surface).unwrap(),
+            width: size.0,
+            height: size.1,
+            // The present_mode uses the wgpu::PresentMode enum which determines how to sync the swap chain with the display. 
+            present_mode: wgpu::PresentMode::Fifo,
+        };
+
+        let swap_chain = device.create_swap_chain(&surface, &sc_desc);
+
+        Self {
+            surface,
+            device,
+            queue,
+            sc_desc,
+            swap_chain,
+            size
+        }
+    }
+
+    // impl State
+    fn Resize(&mut self, new_size: (u32, u32)) {
+        self.size = new_size;
+        self.sc_desc.width = new_size.0;
+        self.sc_desc.height = new_size.1;
+        self.swap_chain = self.device.create_swap_chain(&self.surface, &self.sc_desc);
+    }
+
+    fn Render(&mut self, gameState: &GameState) -> Result<(), wgpu::SwapChainError> {
+        let frame = self
+            .swap_chain
+            .get_current_frame()?
+            .output;
+
+        let mut encoder = self.device.create_command_encoder(&wgpu::CommandEncoderDescriptor {
+            label: Some("Render Encoder"),
+        });
+
+        let render_pass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
+            label: Some("Render Pass"),
+            color_attachments: &[
+                wgpu::RenderPassColorAttachment {
+                    view: &frame.view,
+                    resolve_target: None,
+                    ops: wgpu::Operations {
+                        load: wgpu::LoadOp::Clear(gameState.clearColor),
+                        store: true,
+                    }
+                }
+            ],
+            depth_stencil_attachment: None,
+        });
+
+        drop(render_pass);
+
+        // Finish the command buffer, and to submit it to the gpu's render queue.
+        self.queue.submit(std::iter::once(encoder.finish()));
+
+        Ok(())
+    }
+}
 
 struct Input {
     currentPressedKeys: HashSet<sdl2::keyboard::Scancode>,
@@ -13,8 +122,8 @@ struct Input {
 }
 
 impl Input {
-    pub fn new(eventPump: &sdl2::EventPump) -> Input {
-        Input {
+    pub fn new(eventPump: &sdl2::EventPump) -> Self {
+        Self {
             currentPressedKeys: eventPump.keyboard_state().pressed_scancodes().collect(),
             previousPressedKeys: eventPump.keyboard_state().pressed_scancodes().collect()
         }
@@ -40,6 +149,7 @@ impl Input {
 
 struct AppState {
     input: Input,
+    graphics: Graphics,
     startOfFrame: Instant,
     timeElapsed: f64,
     deltaTime: f64,
@@ -47,11 +157,16 @@ struct AppState {
     exitApp: bool
 }
 
+struct GameState {
+    clearColor: wgpu::Color
+}
+
 impl AppState {
-    pub fn new(input: Input, targerFPS: Option<u16>) -> AppState {
+    pub fn new(input: Input, graphics: Graphics, targerFPS: Option<u16>) -> AppState {
         let fps = targerFPS.unwrap_or(60);
         AppState {
             input: input,
+            graphics: graphics,
             startOfFrame: Instant::now(),
             timeElapsed: 0.0,
             targetFPS: fps,
@@ -65,23 +180,23 @@ impl AppState {
     }
 }
 
-fn UpdateGame(appState: &AppState, color: &mut Color) {
-    if appState.input.IsKeyDown(sdl2::keyboard::Scancode::A) && color.r > 1 {
-        color.r -= 127;
+fn UpdateGame(appState: &AppState, gameState: &mut GameState) {
+    if appState.input.IsKeyDown(sdl2::keyboard::Scancode::R) {
+        gameState.clearColor.r = 1.0;
+        gameState.clearColor.g = 0.0;
+        gameState.clearColor.b = 0.0;
     }
 
-    if appState.input.IsKeyPressed(sdl2::keyboard::Scancode::S) {
-        color.g = 0;
+    if appState.input.IsKeyDown(sdl2::keyboard::Scancode::G) {
+        gameState.clearColor.r = 0.0;
+        gameState.clearColor.g = 1.0;
+        gameState.clearColor.b = 0.0;
     }
 
-    if appState.input.IsKeyUp(sdl2::keyboard::Scancode::D) {
-        color.b = 0;
-    }
-
-    if appState.input.IsKeyUp(sdl2::keyboard::Scancode::R) {
-        color.r = 255;
-        color.g = 255;
-        color.b = 255;
+    if appState.input.IsKeyDown(sdl2::keyboard::Scancode::B) {
+        gameState.clearColor.r = 0.0;
+        gameState.clearColor.g = 0.0;
+        gameState.clearColor.b = 1.0;
     }
 }
 
@@ -92,9 +207,11 @@ fn EnterFrame(eventPump: &mut sdl2::EventPump, appState: &mut AppState) {
     for event in eventPump.poll_iter() {
         match event {
             Event::Quit {..} |
-            Event::KeyDown { keycode: Some(Keycode::Escape), .. } => 
-            {
+            Event::KeyDown { keycode: Some(Keycode::Escape), .. } =>  {
                 appState.exitApp = true;
+            },
+            Event::Window { win_event : sdl2::event::WindowEvent::Resized(width, height), .. }=> {
+                appState.graphics.Resize((width as u32, height as u32));
             },
             _ => {}
         }
@@ -121,13 +238,22 @@ fn main() {
     let window = videoSubsystem
         .window("Sample", 800, 800)
         .position_centered()
+        .resizable()
         .build()
         .unwrap();
 
-    let mut canvas = window.into_canvas().build().unwrap();
+    // let mut canvas = window.into_canvas().build().unwrap();
     let mut eventPump = sdlContext.event_pump().unwrap();
-    let mut appState = AppState::new(Input::new(&eventPump), None);
-    let mut color = Color::RGB(255, 255, 255); 
+    let graphics = block_on(Graphics::new(&window));
+    let mut appState = AppState::new(Input::new(&eventPump), graphics, None);
+    let mut gameState = GameState {
+        clearColor: wgpu::Color {
+            r: 0.1,
+            g: 0.2,
+            b: 0.3,
+            a: 1.0
+        }
+    };
 
     'gameloop: loop {
         EnterFrame(&mut eventPump, &mut appState);
@@ -136,11 +262,17 @@ fn main() {
             break 'gameloop;
         }
 
-        UpdateGame(&appState, &mut color);
+        UpdateGame(&appState, &mut gameState);
 
-        canvas.set_draw_color(color);
-        canvas.clear();
-        canvas.present();
+        match appState.graphics.Render(&gameState) {
+            Ok(_) => {}
+            // Recreate the swap_chain if lost
+            Err(wgpu::SwapChainError::Lost) => appState.graphics.Resize(appState.graphics.size),
+            // The system is out of memory, we should probably quit
+            Err(wgpu::SwapChainError::OutOfMemory) => appState.exitApp = true,
+            // All other errors (Outdated, Timeout) should be resolved by the next frame
+            Err(e) => eprintln!("{:?}", e),
+        }
 
         ExitFrame(&mut appState);
     }
